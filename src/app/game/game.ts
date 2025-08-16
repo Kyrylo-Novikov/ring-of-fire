d827ef5b24859dd460e6d001745882957b462dba
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-08-16 17:58:56 +0200

    feat(game): add game logic and place player on board

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GameCurrentInfos } from './../../models/game_model';
+import { Player } from '../player/player';
 
 @Component({
   selector: 'app-game',
-  imports: [CommonModule],
+  imports: [CommonModule, Player],
   templateUrl: './game.html',
   styleUrl: './game.scss',
 })
-export class Game {
+export class Game implements OnInit {
+  currentGame: GameCurrentInfos = new GameCurrentInfos();
+  currentCard: string = '';
+
   pickCardAnimation = false;
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.currentGame = new GameCurrentInfos();
+  }
+
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.currentGame.stack.pop() || '';
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.currentGame.playedCards.push(this.currentCard);
+        setTimeout(() => {
+          this.pickCardAnimation = false;
+        }, 10);
+      }, 1000);
+    }
   }
 }
